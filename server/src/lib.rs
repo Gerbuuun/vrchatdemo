eabfc976a95df52b9f8de0468079e9ddafbba107
doodlechaos <merobbins5@gmail.com>
2025-04-06 17:21:42 -0500

    Basic rust server steup. Player table, position, and rotation.

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,34 +1,74 @@
+pub mod math;
+
+use math::DbVector2;
 use spacetimedb::{ReducerContext, Table};
 
-#[spacetimedb::table(name = person)]
-pub struct Person {
-    name: String
-}
+#[spacetimedb::table(name = player, public)]
+#[spacetimedb::table(name = logged_out_player, public)]
+#[derive(Clone, Debug)]
+pub struct Player{
+    #[primary_key]
+    identity: spacetimedb::Identity,
 
-#[spacetimedb::reducer(init)]
-pub fn init(_ctx: &ReducerContext) {
-    // Called when the module is initially published
+    #[unique]
+    #[auto_inc]
+    player_id: u32,
+
+    username: Option<String>,
+
+    pub position: DbVector2,
+    pub rotation_yaw: f32,
+
 }
 
 #[spacetimedb::reducer(client_connected)]
-pub fn identity_connected(_ctx: &ReducerContext) {
-    // Called everytime a new client connects
+pub fn connect(ctx: &ReducerContext) -> Result<(), String> {
+    if let Some(player) = ctx.db.logged_out_player().identity().find(&ctx.sender) {
+        ctx.db.player().insert(player.clone());
+        ctx.db
+            .logged_out_player()
+            .identity()
+            .delete(&player.identity);
+    } else {
+        ctx.db.player().try_insert(Player {
+            identity: ctx.sender,
+            player_id: 0,
+            username: None,
+            position: DbVector2::new(0.0, 0.0),
+            rotation_yaw: 0.0,
+        })?;
+    }
+    Ok(())
 }
 
 #[spacetimedb::reducer(client_disconnected)]
-pub fn identity_disconnected(_ctx: &ReducerContext) {
-    // Called everytime a client disconnects
+pub fn disconnect(ctx: &ReducerContext) -> Result<(), String> {
+    let player = ctx
+        .db
+        .player()
+        .identity()
+        .find(&ctx.sender)
+        .ok_or("Player not found")?;
+
+    let player_id = player.player_id;
+    ctx.db.logged_out_player().insert(player);
+    ctx.db.player().identity().delete(&ctx.sender);
+
+    Ok(())
 }
 
-#[spacetimedb::reducer]
-pub fn add(ctx: &ReducerContext, name: String) {
-    ctx.db.person().insert(Person { name });
-}
 
+//Allowing the clients to be authoritative for now (For demonstration purposes only)
 #[spacetimedb::reducer]
-pub fn say_hello(ctx: &ReducerContext) {
-    for person in ctx.db.person().iter() {
-        log::info!("Hello, {}!", person.name);
+pub fn update_player_position(ctx: &ReducerContext, position: DbVector2, rotation: f32) {
+    
+    if let Some(mut player) = ctx.db.player().identity().find(&ctx.sender){
+        player.position = position;
+        player.rotation_yaw = rotation;
+        ctx.db.player().identity().update(player);
+        log::info!("Updated player position");
     }
-    log::info!("Hello, World!");
+    else{
+        log::error!("Player not found");
+    }
 }
